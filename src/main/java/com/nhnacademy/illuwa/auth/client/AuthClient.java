27fa2aa7b17458e31bff28da871079ab1b6d6cf4
pear_c <zxcv9676@naver.com>
2025-06-30 14:41:36 +0900

    fix: 회원가입 로직 수정

---
+++ src/main/java/com/nhnacademy/illuwa/auth/client/AuthClient.java
@@ -0,0 +1,12 @@
+package com.nhnacademy.illuwa.auth.client;
+
+import com.nhnacademy.illuwa.auth.dto.MemberRegisterRequest;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@FeignClient(name = "auth-service", url = "${api.base-url}")
+public interface AuthClient {
+
+    @PostMapping("/auth/signup")
+    void signup(MemberRegisterRequest request);
+}
