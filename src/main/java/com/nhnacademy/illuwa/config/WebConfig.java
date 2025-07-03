246b0c2fc59a09ac4a170196249ffd65327866a9
RiveroadKim <kanggil0825@gmail.com>
2025-07-03 13:13:35 +0900

    feat: 로그인 상태에 대한 header 변환

--- src/main/java/com/nhnacademy/illuwa/config/WebConfig.java
+++ src/main/java/com/nhnacademy/illuwa/config/WebConfig.java
@@ -1,11 +1,28 @@
 package com.nhnacademy.illuwa.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    private final AuthInterceptor authInterceptor;
+
+    @Autowired
+    public WebConfig(AuthInterceptor authInterceptor) {
+        this.authInterceptor = authInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/css/**", "/js/**", "/images/**");
+    }
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/static/**")
