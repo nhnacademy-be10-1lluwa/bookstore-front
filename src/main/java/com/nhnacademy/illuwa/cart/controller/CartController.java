039ceacffdef7ce104b8f312410828de5e0d5432
{haini000} <{haini00)@naver.com}>
2025-07-07 13:58:04 +0900

    feat: 장바구니 구현중

--- src/main/java/com/nhnacademy/illuwa/cart/controller/CartController.java
+++ src/main/java/com/nhnacademy/illuwa/cart/controller/CartController.java
@@ -1,12 +1,35 @@
 package com.nhnacademy.illuwa.cart.controller;
 
+import com.nhnacademy.illuwa.cart.dto.BookCartResponse;
+import com.nhnacademy.illuwa.cart.dto.CartResponse;
+import com.nhnacademy.illuwa.cart.service.CartService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
+@RequiredArgsConstructor
 @Controller
 public class CartController {
+
+    private final CartService cartService;
+
     @GetMapping("/cart")
-    public String cart() {
+    public String getCart(Model model) {
+
+        CartResponse cartResponse = cartService.getCart();
+        List<BookCartResponse> books = cartResponse.getBooks();
+
+        model.addAttribute("books", books);
+
         return "user/cart";
+    }
+
+    @DeleteMapping("/cart")
+    public void deleteCart() {
+        cartService.clearCart();
     }
 }
---
+++ src/main/java/com/nhnacademy/illuwa/cart/service/CartService.java
@@ -0,0 +1,22 @@
+package com.nhnacademy.illuwa.cart.service;
+
+import com.nhnacademy.illuwa.book.client.ProductServiceClient;
+import com.nhnacademy.illuwa.cart.dto.CartResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class CartService {
+
+    private final ProductServiceClient productServiceClient;
+
+
+    public CartResponse getCart() {
+        return productServiceClient.getCart();
+    }
+
+    public void clearCart() {
+        productServiceClient.cleanCart();
+    }
+}
