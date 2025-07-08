324f73ffee814e6a8517ebaea0bb49e222f37760
koreanguyyyy <park6049612@naver.com>
2025-07-08 18:33:39 +0900

    feat: 주문 조회 client 코드 추가

--- src/main/java/com/nhnacademy/illuwa/order/client/OrderServiceClient.java
+++ src/main/java/com/nhnacademy/illuwa/order/client/OrderServiceClient.java
@@ -1,10 +1,13 @@
 package com.nhnacademy.illuwa.order.client;
 
+import com.nhnacademy.illuwa.order.dto.OrderResponse;
 import com.nhnacademy.illuwa.order.dto.PackagingRequestDto;
 import com.nhnacademy.illuwa.order.dto.PackagingResponseDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -15,4 +18,7 @@ public interface OrderServiceClient {
 
     @PostMapping("/api/packaging")
     PackagingResponseDto createPackaging(PackagingRequestDto packagingRequestDto);
+
+    @GetMapping("/order/guest/order-history/{orderNumber}")
+    OrderResponse getGuestOrderHistory(@PathVariable("orderNumber") String orderNumber, @RequestParam("contact") String recipientContact);
 }
