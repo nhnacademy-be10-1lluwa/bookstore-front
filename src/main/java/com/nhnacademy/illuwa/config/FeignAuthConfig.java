97c630bddde87a54ff7581fbf0352ba2f899da1e
JunHyeon <tempjun0215@gmail.com>
2025-07-10 16:42:29 +0900

    -

--- src/main/java/com/nhnacademy/illuwa/config/FeignAuthConfig.java
+++ src/main/java/com/nhnacademy/illuwa/config/FeignAuthConfig.java
@@ -13,30 +13,30 @@ import java.util.List;
 
 @Configuration
 public class FeignAuthConfig {
-//    // 모든 Feign 요청에 Authorization 헤더 주입
-//    @Bean
-//    public RequestInterceptor feignAuthInterceptor() {
-//        return template -> {
-//            // 화이트 리스트 경로
-//            List<String> whiteList = List.of("/books/bestseller");
-//
-//            String url = template.path();
-//
-//            boolean isWhiteListed = whiteList.stream().anyMatch(url::startsWith);
-//            if (isWhiteListed) {
-//                return;
-//            }
-//
-//            ServletRequestAttributes attr = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
-//            if (attr == null) {
-//                return;
-//            }
-//
-//            HttpServletRequest request = attr.getRequest();
-//            Cookie cookie = WebUtils.getCookie(request, "ACCESS_TOKEN");
-//            if (cookie != null) {
-//                template.header("Authorization", "Bearer " + cookie.getValue());
-//            }
-//        };
-//    }
+    // 모든 Feign 요청에 Authorization 헤더 주입
+    @Bean
+    public RequestInterceptor feignAuthInterceptor() {
+        return template -> {
+            // 화이트 리스트 경로
+            List<String> whiteList = List.of("/books/bestseller");
+
+            String url = template.path();
+
+            boolean isWhiteListed = whiteList.stream().anyMatch(url::startsWith);
+            if (isWhiteListed) {
+                return;
+            }
+
+            ServletRequestAttributes attr = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+            if (attr == null) {
+                return;
+            }
+
+            HttpServletRequest request = attr.getRequest();
+            Cookie cookie = WebUtils.getCookie(request, "ACCESS_TOKEN");
+            if (cookie != null) {
+                template.header("Authorization", "Bearer " + cookie.getValue());
+            }
+        };
+    }
 }
