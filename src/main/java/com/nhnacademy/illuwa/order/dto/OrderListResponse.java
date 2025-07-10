5aad4f2a94ee63974d092b5426477e02f5b51574
koreanguyyyy <park6049612@naver.com>
2025-07-10 10:39:04 +0900

    refactor: 주문 상태 내역 dto 수정

--- src/main/java/com/nhnacademy/illuwa/order/dto/OrderListResponse.java
+++ src/main/java/com/nhnacademy/illuwa/order/dto/OrderListResponse.java
@@ -14,6 +14,7 @@ import java.time.LocalDateTime;
 public class OrderListResponse {
 
     private Long orderId;
+    private String orderNumber;
     private LocalDateTime orderDate;
     private BigDecimal totalPrice;
     private OrderStatus orderStatus;
