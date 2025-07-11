6d549a56ee6e3eacfe72a30865c610065f82795b
JunHyeon <tempjun0215@gmail.com>
2025-07-11 21:42:29 +0900

    cleanup: 안쓰는 import 삭제

--- src/main/java/com/nhnacademy/illuwa/config/FeignClientConfig.java
+++ src/main/java/com/nhnacademy/illuwa/config/FeignClientConfig.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
 import org.springframework.cloud.openfeign.support.SpringEncoder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
 import feign.form.spring.SpringFormEncoder;
 import feign.codec.Encoder;
 
