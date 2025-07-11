2ff607a7f36f2ac0b1964939a9b6843ef2c3fa16
aaaaaattt <chlgur13m@naver.com>
2025-07-12 04:33:06 +0900

    feat(book): - 도서 부가 정보 response DTO

---
+++ src/main/java/com/nhnacademy/illuwa/book/dto/BookDetailWithExtraInfoResponse.java
@@ -0,0 +1,34 @@
+package com.nhnacademy.illuwa.book.dto;
+
+import groovy.transform.builder.Builder;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BookDetailWithExtraInfoResponse {
+    private Long id;
+    private String title;
+    private String contents;
+    private String description;
+    private String author;
+    private String publisher;
+    private String publishedDate;  // String 타입으로 날짜
+    private String isbn;
+    private BigDecimal regularPrice;
+    private BigDecimal salePrice;
+    private boolean giftwrap;
+    private Integer count;
+    private String imgUrl;
+    private String status;  // 예: "AVAILABLE", "UNAVAILABLE"
+    private Long categoryId;
+    private Long level1;
+    private Long level2;
+}
