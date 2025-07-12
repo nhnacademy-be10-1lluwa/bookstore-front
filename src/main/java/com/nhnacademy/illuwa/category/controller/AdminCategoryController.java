d307ae710eb12f926591c20528fb8f964a8a0ba8
aaaaaattt <chlgur13m@naver.com>
2025-07-12 14:09:11 +0900

    feat(category): pagination api 추가

--- src/main/java/com/nhnacademy/illuwa/category/controller/AdminCategoryController.java
+++ src/main/java/com/nhnacademy/illuwa/category/controller/AdminCategoryController.java
@@ -4,6 +4,7 @@ import com.nhnacademy.illuwa.book.dto.CategoryResponse;
 import com.nhnacademy.illuwa.category.client.CategoryServiceClient;
 import com.nhnacademy.illuwa.category.dto.CategoryCreateRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -37,4 +38,27 @@ public class AdminCategoryController {
             categoryServiceClient.deleteCategory(id);
         return "redirect:/admin/category/manage";
     }
+
+
+    @GetMapping("/manage")
+    public String categoryManagePage(@RequestParam(defaultValue = "0") int page,
+                                     @RequestParam(defaultValue = "10") int size,
+                                     @RequestParam(defaultValue = "id,asc") String sort,
+                                     Model model) {
+        // 페이징된 목록
+        Page<CategoryFlatResponse> categoryPage = categoryServiceClient.getFlatCategoriesPaged(page, size, sort);
+
+        // 드롭다운
+        Page<CategoryFlatResponse> allPage = categoryServiceClient.getFlatCategoriesPaged(0, 100, sort);
+        List<CategoryFlatResponse> allCategories = allPage.getContent();
+
+        model.addAttribute("categoryPage", categoryPage);
+        model.addAttribute("allCategories", allCategories);
+        model.addAttribute("newCategory", new CategoryCreateRequest());
+
+        return "admin/category/manage";
+    }
+
+
+
 }
