c1ee8e5bdc883f106ef8f9634955e10e36bded9e
aaaaaattt <chlgur13m@naver.com>
2025-07-13 16:59:20 +0900

    feat(category): 태그 DTO 추가 - Create - Response( Prodect-service )

---
+++ src/main/java/com/nhnacademy/illuwa/tag/dto/TagCreateRequest.java
@@ -0,0 +1,14 @@
+package com.nhnacademy.illuwa.tag.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class TagCreateRequest {
+    private String name;
+}
