06f23addf33d5973b66a612c8f644a0cf78ab83f
CGE <bestofGE@naver.com>
2025-07-14 22:41:18 +0900

    feat: 관리자 회원목록 조회, 등급별 포인트 적립 구현

--- src/main/java/com/nhnacademy/illuwa/member/service/AdminMemberService.java
+++ src/main/java/com/nhnacademy/illuwa/member/service/AdminMemberService.java
@@ -1,18 +1,27 @@
 package com.nhnacademy.illuwa.member.service;
 
 import com.nhnacademy.illuwa.common.dto.PageResponse;
+import com.nhnacademy.illuwa.grade.enums.GradeName;
 import com.nhnacademy.illuwa.member.client.AdminMemberServiceClient;
 import com.nhnacademy.illuwa.member.dto.MemberResponse;
+import com.nhnacademy.illuwa.pointhistory.dto.PointHistoryResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.math.BigDecimal;
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
 public class AdminMemberService {
     private final AdminMemberServiceClient adminMemberServiceClient;
 
-    public PageResponse<MemberResponse> getPagedMemberList(@RequestParam("page") int page, @RequestParam("size") int size) {
-        return adminMemberServiceClient.getPagedMemberList(page, size);
+    public PageResponse<MemberResponse> getPagedMemberList(@RequestParam(value = "grade", required = false) GradeName gradeName, @RequestParam("page") int page, @RequestParam("size") int size) {
+        return adminMemberServiceClient.getPagedMemberListFilteredByGrade(gradeName, page, size);
+    }
+
+    public List<PointHistoryResponse> givePointToGrade(@RequestParam(value = "grade", required = false) GradeName gradeName, @RequestParam(value = "point", required = false) BigDecimal point){
+       return adminMemberServiceClient.givePointToGrade(gradeName, point);
     }
 }
