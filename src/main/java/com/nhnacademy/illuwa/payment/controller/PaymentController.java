e5c3cad03e54384d063a3129f1d220f45dd942fa
pear_c <zxcv9676@naver.com>
2025-07-15 21:28:32 +0900

    feat: 주문 결제 완료 구현

--- src/main/java/com/nhnacademy/illuwa/payment/controller/PaymentController.java
+++ src/main/java/com/nhnacademy/illuwa/payment/controller/PaymentController.java
@@ -2,9 +2,11 @@ package com.nhnacademy.illuwa.payment.controller;
 
 import com.nhnacademy.illuwa.payment.client.PaymentServiceClient;
 import com.nhnacademy.illuwa.payment.dto.PaymentConfirmRequest;
+import com.nhnacademy.illuwa.payment.dto.PaymentResponse;
 import feign.FeignException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,17 +23,18 @@ public class PaymentController {
     public String handlePaymentSuccess(@RequestParam(name = "orderId") String orderNumber,
                                        @RequestParam(name = "paymentKey") String paymentKey,
                                        @RequestParam(name = "amount") int amount,
-                                       RedirectAttributes redirectAttributes) {
+                                       Model model) {
 
         PaymentConfirmRequest request = new PaymentConfirmRequest(orderNumber, paymentKey, amount);
 
         try {
-            paymentServiceClient.confirmPayment(request);
-            redirectAttributes.addFlashAttribute("message", "결제가 완료되었습니다.");
-            return "redirect:/order/complete/" + orderNumber;
+            PaymentResponse paymentResponse = paymentServiceClient.confirmPayment(request);
+
+            model.addAttribute("payment", paymentResponse);
+
+            return "payment/success";
         } catch (FeignException e) {
-            redirectAttributes.addFlashAttribute("error", "결제 승인 실패: " + e.getMessage());
-            return "redirect:/payment/fail";
+            return "payment/fail";
         }
     }
 }
---
+++ src/main/java/com/nhnacademy/illuwa/payment/dto/PaymentResponse.java
@@ -0,0 +1,35 @@
+package com.nhnacademy.illuwa.payment.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.OffsetDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class PaymentResponse {
+    private String paymentKey;
+    private String orderId;
+    private String orderName;
+    private String status;
+    // 요청시간
+    private OffsetDateTime requestedAt;
+    // 승인사간
+    private OffsetDateTime approvedAt;
+    private int totalAmount;
+
+    private CardInfo card;
+
+    @Getter
+    @NoArgsConstructor
+    @Setter
+    public static class CardInfo {
+        private String issuerCode;
+        private String acquirerCode;
+        private String number;
+        private String cardType;
+        private int amount;
+    }
+}
