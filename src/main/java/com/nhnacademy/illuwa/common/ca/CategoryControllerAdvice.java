26718420cb2a42cbc3ff28d0b66d2699aeb63afe
aaaaaattt <chlgur13m@naver.com>
2025-07-16 21:35:58 +0900

    fix(category): 카테고리 controller endpoint 추가

---
+++ src/main/java/com/nhnacademy/illuwa/common/ca/CategoryControllerAdvice.java
@@ -0,0 +1,21 @@
+package com.nhnacademy.illuwa.common.ca;
+
+import com.nhnacademy.illuwa.category.dto.CategoryResponse;
+import com.nhnacademy.illuwa.category.service.CategoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import java.util.List;
+
+@ControllerAdvice
+@RequiredArgsConstructor
+public class CategoryControllerAdvice {
+
+    private final CategoryService categoryService;
+
+    @ModelAttribute("categories")
+    public List<CategoryResponse> populateCategories() {
+        return categoryService.getAllCategories();
+    }
+}
