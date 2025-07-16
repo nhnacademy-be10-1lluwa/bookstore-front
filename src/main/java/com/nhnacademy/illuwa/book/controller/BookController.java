4bfea1e63fbc69af4b35bcd7a6fb36ac7be307dd
JunHyeon <tempjun0215@gmail.com>
2025-07-16 12:44:35 +0900

    feat: 리뷰관련 modelattribute 추가

--- src/main/java/com/nhnacademy/illuwa/book/controller/BookController.java
+++ src/main/java/com/nhnacademy/illuwa/book/controller/BookController.java
@@ -1,14 +1,10 @@
 package com.nhnacademy.illuwa.book.controller;
 
-import com.nhnacademy.illuwa.book.client.ProductServiceClient;
 import com.nhnacademy.illuwa.book.dto.BookDetailResponse;
 import com.nhnacademy.illuwa.book.service.BookService;
 import com.nhnacademy.illuwa.common.dto.PageResponse;
-import com.nhnacademy.illuwa.member.dto.MemberResponse;
-import com.nhnacademy.illuwa.member.service.MemberService;
 import com.nhnacademy.illuwa.review.dto.ReviewResponse;
 import com.nhnacademy.illuwa.review.service.ReviewService;
-import feign.FeignException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -19,11 +15,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 @Controller
 @RequiredArgsConstructor
 public class BookController {
-    MemberService memberService;
-    BookService bookService;
-    ReviewService reviewService;
+    private final BookService bookService;
+    private final ReviewService reviewService;
 
-    @GetMapping("/user/book_info/{isbn}")
+    @GetMapping("/user/book-info/{isbn}")
     public String bookInfo(@PathVariable String isbn, Model model) {
         return "book/book_info";
     }
@@ -31,12 +26,7 @@ public class BookController {
 
     @GetMapping("/user/books/{isbn}")
     public String bookDetail(@PathVariable("isbn") String isbn, Model model) {
-        try {
-            MemberResponse member = memberService.getMember();
-            model.addAttribute("currentUser", member.getMemberId()); // 로그인 됨
-        } catch (FeignException.Unauthorized e) {
-            model.addAttribute("currentUser", null); // 로그인 안 됨
-        }
+
         BookDetailResponse bookDetail = bookService.findBookByIsbn(isbn);
 
         Long bookId = bookDetail.getId();
@@ -44,10 +34,8 @@ public class BookController {
 
         model.addAttribute("book", bookDetail);
         model.addAttribute("reviewContent", reviewPage.content());
-        model.addAttribute("reviewTotalPages", reviewPage.totalPages());
         model.addAttribute("reviewPage", reviewPage);
 
         return "book/detail";
     }
-
 }
