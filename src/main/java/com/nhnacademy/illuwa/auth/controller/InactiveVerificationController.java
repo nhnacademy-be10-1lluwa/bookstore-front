9050afb5c6e20c79b8dd63fe2bd9dc9c0359ab3b
pear_c <zxcv9676@naver.com>
2025-07-18 15:42:27 +0900

    refactor: auth-service Front URI 수정

--- src/main/java/com/nhnacademy/illuwa/auth/controller/InactiveVerificationController.java
+++ src/main/java/com/nhnacademy/illuwa/auth/controller/InactiveVerificationController.java
@@ -1,32 +1,30 @@
 package com.nhnacademy.illuwa.auth.controller;
 
 import com.nhnacademy.illuwa.auth.client.InactiveVerificationServiceClient;
-import com.nhnacademy.illuwa.auth.dto.message.SendMessageResponse;
 import com.nhnacademy.illuwa.auth.dto.message.SendVerificationRequest;
 import com.nhnacademy.illuwa.auth.dto.message.VerifyCodeRequest;
-import com.nhnacademy.illuwa.auth.dto.message.VerifyCodeResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequiredArgsConstructor
-@RequestMapping("/proxy/members/inactive")
+@RequestMapping("/verifications/inactive")
 public class InactiveVerificationController {
 
     private final InactiveVerificationServiceClient inactiveVerificationServiceClient;
 
     /** 인증번호 발송 */
     @PostMapping("/code")
-    public SendMessageResponse send(@RequestBody SendVerificationRequest req) {
-        return inactiveVerificationServiceClient.sendCode(req);
+    public void send(@RequestBody SendVerificationRequest req) {
+        inactiveVerificationServiceClient.sendCode(req);
     }
 
     /** 인증번호 검증 */
-    @PostMapping("/verification")
-    public VerifyCodeResponse verify(@RequestBody VerifyCodeRequest req) {
-        return inactiveVerificationServiceClient.verify(req);
+    @PostMapping("/verify")
+    public void verify(@RequestBody VerifyCodeRequest req) {
+        inactiveVerificationServiceClient.verify(req);
     }
 }
