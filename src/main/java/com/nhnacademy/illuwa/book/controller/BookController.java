87b36059344e1294be4535afbd14e9e16f953ef2
aaaaaattt <chlgur13m@naver.com>
2025-07-18 11:27:11 +0900

    feat(search): 도서 상세페이지 api 추가

--- src/main/java/com/nhnacademy/illuwa/book/controller/BookController.java
+++ src/main/java/com/nhnacademy/illuwa/book/controller/BookController.java
@@ -9,46 +9,60 @@ import com.nhnacademy.illuwa.review.dto.ReviewResponse;
 import com.nhnacademy.illuwa.review.service.ReviewService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import java.util.List;
-import java.util.Map;
+    import java.util.List;
+    import java.util.Map;
 
-@Controller
-@RequiredArgsConstructor
-public class BookController {
-    private final BookService bookService;
-    private final ReviewService reviewService;
-    private final MemberService memberService;
+    @Controller
+    @RequiredArgsConstructor
+    @Slf4j
+    public class BookController {
+        private final BookService bookService;
+        private final ReviewService reviewService;
+        private final MemberService memberService;
 
-    @GetMapping("/user/book-info/{isbn}")
-    public String bookInfo(@PathVariable String isbn, Model model) {
-        return "book/book_info";
-    }
+        @GetMapping("/user/book-info/{isbn}")
+        public String bookInfo(@PathVariable String isbn, Model model) {
+            return "book/book_info";
+        }
 
 
-    //accessToken 검증하는 공통 서비스 이용하기
-    @GetMapping("/user/books/{isbn}")
-    public String bookDetail(@PathVariable("isbn") String isbn, Model model,
-                             HttpServletRequest request) {
-        boolean isLoginUser = JwtCookieUtil.checkAccessToken(request);
+        //accessToken 검증하는 공통 서비스 이용하기
+        @GetMapping("/user/books/{isbn}")
+        public String bookDetail(@PathVariable("isbn") String isbn, Model model,
+                                 HttpServletRequest request) {
+            boolean isLoginUser = JwtCookieUtil.checkAccessToken(request);
 
-        BookDetailResponse bookDetail = bookService.findBookByIsbn(isbn);
+            BookDetailResponse bookDetail = bookService.findBookByIsbn(isbn);
 
-        Long bookId = bookDetail.getId();
-//        PageResponse<ReviewResponse> reviewPage = reviewService.getReviewPages(bookId, 0, 5);
-//        List<Long> memberIds = reviewPage.content().stream().map(ReviewResponse::getMemberId).toList();
-//        Map<Long,String> nameMap = memberService.getNamesFromIdList(memberIds);
+            Long bookId = bookDetail.getId();
+    //        PageResponse<ReviewResponse> reviewPage = reviewService.getReviewPages(bookId, 0, 5);
+    //        List<Long> memberIds = reviewPage.content().stream().map(ReviewResponse::getMemberId).toList();
+    //        Map<Long,String> nameMap = memberService.getNamesFromIdList(memberIds);
 
-        model.addAttribute("book", bookDetail);
-//        model.addAttribute("reviewContent", reviewPage.content());
-//        model.addAttribute("reviewPage", reviewPage);
-//        model.addAttribute("nameMap", nameMap);
-//        model.addAttribute("activeMenu", "");
+            model.addAttribute("book", bookDetail);
+    //        model.addAttribute("reviewContent", reviewPage.content());
+    //        model.addAttribute("reviewPage", reviewPage);
+    //        model.addAttribute("nameMap", nameMap);
+    //        model.addAttribute("activeMenu", "");
 
-        return "book/detail";
-    }
+            return "book/detail";
+        }
+
+
+
+        //도서 상세페이지
+        @GetMapping("/books/{id}")
+        public String bookDetailBySearch(@PathVariable("id") Long bookId, Model model){
+
+            BookDetailResponse bookDetail = bookService.findBookById(bookId);
+            model.addAttribute("book", bookDetail);
+
+            return "book/detail";
+        }
 }
