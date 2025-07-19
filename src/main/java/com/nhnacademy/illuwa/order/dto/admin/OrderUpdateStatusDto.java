051f807d25600a559f56d8230365259ddd95303e
koreanguyyyy <park6049612@naver.com>
2025-07-19 19:38:10 +0900

    feat: @Valid 추가

--- src/main/java/com/nhnacademy/illuwa/order/dto/admin/OrderUpdateStatusDto.java
+++ src/main/java/com/nhnacademy/illuwa/order/dto/admin/OrderUpdateStatusDto.java
@@ -1,13 +1,18 @@
 package com.nhnacademy.illuwa.order.dto.admin;
 
 import com.nhnacademy.illuwa.order.dto.types.OrderStatus;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@Valid
 @NoArgsConstructor
 @AllArgsConstructor
 public class OrderUpdateStatusDto {
+
+    @NotNull
     private OrderStatus orderStatus;
 }
--- src/main/java/com/nhnacademy/illuwa/order/dto/orderRequest/OrderItemDto.java
+++ src/main/java/com/nhnacademy/illuwa/order/dto/orderRequest/OrderItemDto.java
@@ -1,6 +1,9 @@
 package com.nhnacademy.illuwa.order.dto.orderRequest;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,11 +14,24 @@ import java.math.BigDecimal;
 @NoArgsConstructor
 @AllArgsConstructor
 public class OrderItemDto {
+    @NotNull
     @JsonProperty("id")
     private Long bookId;
+
+    @NotNull
+    @Positive
     private Integer quantity;
+
+    @NotNull
+    @PositiveOrZero
     private BigDecimal price;
+
+    @NotNull
     private Long packagingId;
+
     private Long couponId;
+
+    @NotNull
+    @PositiveOrZero
     private BigDecimal totalPrice;
 }
