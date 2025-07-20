f1494175b6c2585c15bbf7104f1f97341838f5fa
CGE <bestofGE@naver.com>
2025-07-20 17:19:58 +0900

    feat: 내가 쓴 리뷰 페이지 구현

--- src/main/java/com/nhnacademy/illuwa/review/controller/ReviewController.java
+++ src/main/java/com/nhnacademy/illuwa/review/controller/ReviewController.java
@@ -13,6 +13,10 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 @Controller
 @RequiredArgsConstructor
 public class ReviewController {
@@ -23,6 +27,7 @@ public class ReviewController {
     public String showCreateForm(Model model,
                                  @RequestParam(name = "book-id") long bookId,
                                  @RequestParam(name = "order-id") Long orderId) {
+        model.addAttribute("activeMenu", model.getAttribute("activeMenu"));
         model.addAttribute("mode", "new");
         model.addAttribute("bookId", bookId);
         model.addAttribute("orderId", orderId);
@@ -34,7 +39,7 @@ public class ReviewController {
     @GetMapping("/reviews/{review-id}")
     public String showDetail(@RequestParam(name = "book-id") long bookId,
                              @PathVariable(name = "review-id") long reviewId,
-                             @RequestParam(name = "order-id") Long orderId,
+                             @RequestParam(name = "order-id", required = false) Long orderId,
                              Model model) {
         ReviewResponse review = reviewService.getReview(bookId, reviewId);
         model.addAttribute("mode", "view");
@@ -93,7 +98,6 @@ public class ReviewController {
         } catch (Exception e) {
             return "review/review_form";
         }
-
         return "redirect:/order-detail/" + orderId;
     }
 
@@ -109,16 +113,22 @@ public class ReviewController {
     // 내가 쓴 리뷰목록 조회 (페이지)
     @GetMapping("/review-history")
     public String getReviewHistory(Model model,
-                                   @RequestParam(name = "book-id") long bookId,
                                  @RequestParam(defaultValue = "0") int page,
                                  @RequestParam(defaultValue = "5") int size) {
-        PageResponse<ReviewResponse> reviewPage = reviewService.getReviewPages(bookId, page, size);
+        PageResponse<ReviewResponse> reviewPage = reviewService.getMemberReviewPages(page, size);
+
+        List<Long> reviewIds = new ArrayList<>();
+        for(ReviewResponse review: reviewPage.content()){
+            reviewIds.add(review.getReviewId());
+        }
+        Map<Long, String> titleList = reviewService.getBookTitlesFromReviewIds(reviewIds);
+
         model.addAttribute("reviewList", reviewPage.content());
+        model.addAttribute("titleList", titleList);
         model.addAttribute("currentPage", page);
         model.addAttribute("pageSize", reviewPage.size());
         model.addAttribute("totalPages", reviewPage.totalPages());
         model.addAttribute("lastPageIndex", Math.max(0, reviewPage.totalPages() - 1));
-        model.addAttribute("bookId", bookId);
         model.addAttribute("activeMenu", "review-history");
         return "mypage/section/review_history";
     }
