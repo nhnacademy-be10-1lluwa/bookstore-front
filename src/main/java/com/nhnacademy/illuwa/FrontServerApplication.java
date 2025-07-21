968eead013aaebfd0a5e31faa946a641dffc842f
aaaaaattt <chlgur13m@naver.com>
2025-07-21 16:24:11 +0900

    feat(category): @EnableCaching 추가

--- src/main/java/com/nhnacademy/illuwa/FrontServerApplication.java
+++ src/main/java/com/nhnacademy/illuwa/FrontServerApplication.java
@@ -2,11 +2,13 @@ package com.nhnacademy.illuwa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @EnableFeignClients
+@EnableCaching
 public class FrontServerApplication {
     public static void main(String[] args) {
         SpringApplication app = new SpringApplication(FrontServerApplication.class);
