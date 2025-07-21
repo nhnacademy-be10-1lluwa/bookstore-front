b1b52cc4b9ae3b1dbeace79d8ccbe6cb0535deac
aaaaaattt <chlgur13m@naver.com>
2025-07-21 16:23:27 +0900

    chore(category): @Cachable 애노테이션 추가

--- src/main/java/com/nhnacademy/illuwa/category/service/CategoryService.java
+++ src/main/java/com/nhnacademy/illuwa/category/service/CategoryService.java
@@ -3,6 +3,7 @@ package com.nhnacademy.illuwa.category.service;
 import com.nhnacademy.illuwa.category.client.CategoryServiceClient;
 import com.nhnacademy.illuwa.category.dto.CategoryResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,6 +14,7 @@ public class CategoryService {
 
     private final CategoryServiceClient categoryClient;
 
+    @Cacheable(value = "categories")
     public List<CategoryResponse> getAllCategories() {
         return categoryClient.getAllCategories();
     }
