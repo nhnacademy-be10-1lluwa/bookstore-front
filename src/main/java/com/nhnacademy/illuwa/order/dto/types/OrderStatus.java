17c67c8ce814f5e130912b504ff3a14b42b0ab67
koreanguyyyy <park6049612@naver.com>
2025-07-22 15:16:58 +0900

    refactor: 배송 상태 한국어 처리

--- src/main/java/com/nhnacademy/illuwa/order/dto/types/OrderStatus.java
+++ src/main/java/com/nhnacademy/illuwa/order/dto/types/OrderStatus.java
@@ -1,38 +1,59 @@
 package com.nhnacademy.illuwa.order.dto.types;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum OrderStatus {
     /**
      * 주문이 생성되었으나 아직 결제가 완료되지 않은 상태.
      */
-    AwaitingPayment,
+    AwaitingPayment("결제 대기"),
 
     /**
      * 결제가 정상 승인되어 물류 출고 준비가 완료된 상태.
      */
-    Pending,
+    Pending("출고 준비 완료"),
 
     /**
      * 택배사에 패키지가 인계되어 운송 중에 있는 상태.
      */
-    Shipped,
+    Shipped("배송 중"),
 
     /**
      * 고객에게 물품이 성공적으로 도착한 상태.
      */
-    Delivered,
+    Delivered("배송 완료"),
 
     /**
      * 고객이 최종 구매를 확정하거나, 배송 완료 후 자동 확정된 상태.
      */
-    Confirmed,
+    Confirmed("구매 확정"),
 
     /**
      * 반품 요청 및 환불이 완료된 상태.
      */
-    Returned,
+    Returned("반품"),
 
     /**
      * 고객 요청 등으로 주문이 취소된 상태.
      */
-    Cancelled
+    Cancelled("주문 취소");
+
+    private final String value;
+
+    OrderStatus(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public static OrderStatus fromStatus(String status) {
+        for (OrderStatus value : OrderStatus.values()) {
+            if (value.getValue().equals(status)) {
+                return value;
+            }
+        }
+        return null;
+    }
 }
